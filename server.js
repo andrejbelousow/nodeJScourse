d6b07281f84f81c686c98cd8e2fd7c9b2f7f687c
Andrey2 <andr.belousow2017@yandex.ru>
2019-11-19 17:53:36 +0300

    404

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extendend: false}));
+
+app.use(adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
+app.listen(3000);
