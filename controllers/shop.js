dac496aaa429f184e79f02ca75d35e2316ac88f6
Andrey2 <andr.belousow2017@yandex.ru>
2019-12-11 18:20:31 +0300

    dfsdsd

---
+++ controllers/shop.js
@@ -0,0 +1,25 @@
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
+  });
+}
