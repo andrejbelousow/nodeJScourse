f073308d7f94f43132583ea1dcd8a23f0eddd4f8
Andrey <andr.belousow2017@yandex.ru>
2020-01-20 22:45:31 +0300

    dsww

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'cradmintre3w'
+const sequelize = new Sequelize('node-complete', 'root', 'cradmintre3w', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
