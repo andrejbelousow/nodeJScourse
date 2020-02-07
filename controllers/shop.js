46bfcae5c715dc53ec106f6b60262653997b8c5b
Andrey2 <andr.belousow2017@yandex.ru>
2020-02-07 19:55:35 +0300

    dsdsd

--- controllers/shop.js
+++ controllers/shop.js
@@ -32,21 +32,17 @@ exports.getIndex = (req, res, next) => {
     }).catch(err => console.log(err));
 };
 
-// exports.getCart = (req, res, next) => {
-//     req.user.getCart()
-//         .then(cart => {
-//             return cart.getProducts()
-//                 .then(products => {
-//                     res.render('shop/cart', {
-//                         path: '/cart',
-//                         pageTitle: 'Your cart',
-//                         products: products
-//                     });
-//                 })
-//                 .catch(err => console.log(err));
-//         })
-//         .catch(err => console.log(err));
-// };
+exports.getCart = (req, res, next) => {
+    req.user.getCart()
+                .then(products => {
+                    res.render('shop/cart', {
+                        path: '/cart',
+                        pageTitle: 'Your cart',
+                        products: products
+                    });
+                })
+                .catch(err => console.log(err));
+}
 
 exports.postCart = (req, res, next) => {
     const prodId = req.body.productId;
