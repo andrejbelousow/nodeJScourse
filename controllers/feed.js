a4654cfa03ad0394387720386f4f2ae5d5344798
Andrey <andr.belousow2017@yandex.ru>
2020-03-15 21:17:06 +0300

    sdswww

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,10 +1,21 @@
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [{
+      _id: '1',
+      title: 'First Post',
+      content: 'This is the first post!',
+      imageUrl: 'images/duck.jpg',
+      creator: {
+          name: 'Andrey'
+      },
+      createdAt: new Date()
+    }]
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
   // Create post in db
   res.status(201).json({
     message: 'Post created successfully!',
